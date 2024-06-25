2607474f91885811ad52662212d6f3f306b85595
ntngon6495 <ntn6495@gmail.com>
2024-06-25 22:19:31 +0700

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	let src = '/bao_tri.jpg';
+	let name = 'Bao tri he thong';
+</script>
+<div class='container'>
+  <img {src} alt={name}  />
+</div>
+
+<style>
+  .container {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+  }
+  img {
+    /* width: 1024px;
+    height: 768px; */
+    max-width: 100%;
+    max-height: 100vh;
+  }
+  @media only screen and (max-width: 600px) {
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+</style>
+
---
+++ src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+
+  const productId  = $page.params.productId;
+
+  onMount(() => {
+    // page.set({ title: 'Product', description: 'Product page' });
+    console.log('productId', productId);
+  });
+</script>
+
+<h1>Product: {productId}</h1>
