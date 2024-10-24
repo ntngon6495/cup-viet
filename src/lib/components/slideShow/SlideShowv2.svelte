65074d1a6fedd84be1da0dc50c5f1919b4412010
NgonNguyen-SungroveTech <ngon.nguyen@sungrove.co.jp>
2024-10-24 22:41:25 +0700

    update slide show

--- src/lib/components/slideShow/SlideShowv2.svelte
+++ src/lib/components/slideShow/SlideShowv2.svelte
@@ -138,6 +138,49 @@
   </section>
   <section class="section">
     <div
+      class="section-bg bg-white"
+    >
+      <div class="relative z-[1]">
+        <div class="flex w-full h-full justify-center">
+          <div class="w-[1250px] flex items-center">
+            <div class="w-3/5 mt-10">
+              <img
+                loading="lazy"
+                decoding="async"
+                src="/images/banner/BANNER-6.png"
+                class="attachment-original size-original 2xl:w-[800px] 2xl:h-[550px] md:w-[600px] md:[300px]"
+                alt=""
+                srcset="/images/banner/BANNER-6.png 900w, /images/banner/BANNER-6.png 700w, /images/banner/BANNER-6.png 768w, /images/banner/BANNER-6.png 800w"
+                sizes="(max-width: 900px) 100vw, 900px"
+              />
+              <!-- <div
+                class="attachment-original size-original 2xl:w-[800px] h-[550px] md:w-[600px] md:[300px]"
+              ></div> -->
+            </div>
+            <div class="col-inner text-right w-2/5">
+              <div id="text-1138206211" class="text">
+                <h2 class="text-[35px] text-category-100">LUXURY CUP</h2>
+                <h3 class="text-[25px] text-[#444444]">MADE FOR GOLF</h3>
+              </div>
+
+              <a
+                href="/category/5"
+                target="_self"
+                class="lowercase text-category-100"
+              >
+                <span class="pb-1 border-b border-b-category-100"
+                  >{`Xem thêm  >`}</span
+                >
+                <i class="icon-angle-right"></i></a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section">
+    <div
       class="bg section-bg fill bg-fill bg-[url('/images/banner/BANNER-PHALE.png')] opacity-100"
     >
       <div class="relative z-[1]">
@@ -188,7 +231,7 @@
     padding-top: 20px;
     padding-bottom: 20px;
     min-height: 550px;
-    background-color: rgb(193, 193, 193);
+    /*background-color: rgb(193, 193, 193);*/
     width: 100%;
     display: -webkit-box;
     display: -ms-flexbox;
